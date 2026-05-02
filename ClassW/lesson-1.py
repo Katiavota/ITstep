443c5727272bfb0d4b6a7ba915281c2f601cc68c
Katiavota <masenkokata1592@gmail.com>
2026-05-02 17:35:19 +0300

    win

--- ClassW/lesson-1.py
+++ ClassW/lesson-1.py
@@ -1,1 +1,18 @@
-print("Hello World")
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def grow(self):
+        self.age += 1
+        print(self.name, "став стрше", self.age)
+
+cat1 = Cat("Киса", 7)
+cat2 = Cat("Барсик", 8)
+
+print(cat1.name)
+print(cat1.age)
+
+print(cat2.name)
+print(cat2.age)
+
+cat1.grow()
---
+++ ClassW/lesson-2.py
@@ -0,0 +1,17 @@
+#num = int(input("число:"))
+#if num > 10:
+    #print("Больше 10")
+#else:
+    #print("Не больше 10")
+
+#num = [1,2,3,4,5,6,7,8,9,10]
+#for i in num:
+    #if i % 2 != 0:
+       #print(i)
+
+num = [1,2,3,4,5,6]
+
+sum = 0
+for i in num:
+    sum += i
+    print("Сумма", sum).
